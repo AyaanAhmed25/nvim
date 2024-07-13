1c05a427cfb59395ad33ceb61f25af66c61d99f8
AyaanAhmed25 <ayaan.ahmed.25@icloud.com>
2024-07-13 15:13:46 -0400

    added oil.nvim

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+    'stevearc/oil.nvim',
+    opts = {
+        delete_to_trash = true,
+    },
+    -- Optional dependencies
+    -- dependencies = { "echasnovski/mini.icons" },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+
+}
+

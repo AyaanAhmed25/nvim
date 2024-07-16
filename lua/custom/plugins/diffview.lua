370ed49429bba062be0808bef166714d78d9941d
AyaanAhmed25 <ayaan.ahmed.25@icloud.com>
2024-07-16 16:00:14 -0400

    added diffview and corresponding keymaps

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+	'sindrets/diffview.nvim',
+	config = function()
+		require('diffview').setup()
+		vim.keymap.set({'n', 'x'}, '<leader>gd',"<cmd>DiffviewOpen<cr>", { desc = '[G]it [D]iff' })
+		vim.keymap.set({'n', 'x'}, '<leader>gh',"<cmd>DiffviewFileHistory<cr>", { desc = '[G]it [H]istory' })
+	end,
+
+
+}

f28be0194992cb3930531c3fde72d3a707f03da7
AyaanAhmed25 <ayaan.ahmed.25@icloud.com>
2024-09-24 22:24:09 -0400

    removed line in lualine file that explicitly set catppuccin as theme, now changing themes changes statusline theme also

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -14,7 +14,7 @@ return {
 		require('lualine').setup {
 			options = {
 				icons_enabled = true,
-				theme = 'catppuccin',
+				-- theme = 'catppuccin',
 				component_separators = { left = '|', right = '|' },
 				section_separators = { left = '', right = '' },
 				disabled_filetypes = {
@@ -42,7 +42,7 @@ return {
 				-- 		return navic.is_available()
 				-- 	end
 				-- }, },
-				lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_x = { 'filetype', --[[ icon={color = {fg='rosewater'}} ]] },
 				lualine_y = { 'progress' },
 				lualine_z = { 'location' }
 			},

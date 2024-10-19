aee91e099d29f29d957c98a13a5a42eed4affe3d
AyaanAhmed25 <ayaan.ahmed.25@icloud.com>
2024-10-19 07:58:22 -0400

    neoscroll to zz
    no longer need neoscroll, only reason i had it was that it was
    disorienting to control d and u, now that it centers it i can tell what
    is happening.

---
+++ lua/custom/DisabledPlugins/neoscroll.lua
@@ -0,0 +1,27 @@
+return {
+  -- "karb94/neoscroll.nvim",
+  -- config = function()
+  --   require('neoscroll').setup({
+  --     mappings = { '<C-u>', '<C-d>', --[[ 'zt', 'zz', 'zb' ]] }
+  --   })
+  --   local neoscroll = require('neoscroll')
+  --   local keymap = {
+  --     ["<C-u>"] = function()
+  --       neoscroll.ctrl_u({ duration = 70 })
+  --       vim.defer_fn(function()
+  --         vim.cmd('normal! zz')
+  --       end, 75) -- Add a slight delay (in milliseconds) to allow scrolling to finish
+  --     end,
+  --     ["<C-d>"] = function()
+  --       neoscroll.ctrl_d({ duration = 70 })
+  --       vim.defer_fn(function()
+  --         vim.cmd('normal! zz')
+  --       end, 75) -- Adjust delay as needed
+  --     end,
+  --   }
+  --   local modes = { 'n', 'v', 'x' }
+  --   for key, func in pairs(keymap) do
+  --     vim.keymap.set(modes, key, func)
+  --   end
+  -- end
+}
